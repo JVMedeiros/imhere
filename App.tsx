5409da67730e25b552ac627b91152821b0ec2786
João Medeiros <jv.medeiros.gallina@gmail.com>
2025-01-25 23:06:46 -0300

    wip: home page wip

--- App.tsx
+++ App.tsx
@@ -1,31 +1,9 @@
-import { Text, StyleSheet, View } from "react-native"
+import Home from "./src/screens/Home"
+
 export default function App() {
   return(
-    <View style={styles.container}>
-      <Text style={styles.eventName}>
-        Data do Evento
-      </Text>
-      <Text style={styles.eventDate}>
-        Sábado, 25 de Janeiro de 2025
-      </Text>
-    </View>
+    <Home />
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#131016',
-    padding: 24
-  },
-  eventName: {
-    color: '#FDFCFE',
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginTop: 48
-  },
-  eventDate: {
-    color: '#6B6B6B',
-    fontSize: 16
-  }
-})
+
