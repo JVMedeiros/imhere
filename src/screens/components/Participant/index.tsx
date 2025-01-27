0de578db21e18628dad395f8316980ad66da4c00
João Medeiros <jv.medeiros.gallina@gmail.com>
2025-01-27 20:22:05 -0300

    feat: add props to participant component

--- src/screens/components/Participant/index.tsx
+++ src/screens/components/Participant/index.tsx
@@ -1,11 +1,15 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 
-export function Participant() {
+interface ParticipantProps {
+  name: string;
+}
+
+export function Participant({ name }: ParticipantProps) {
   return (
     <View style={styles.container}>
       <Text style={styles.name}>
-        João Medeiros
+        {name}
       </Text>
       <TouchableOpacity style={styles.button}>
           <Text style={styles.buttonText}>
